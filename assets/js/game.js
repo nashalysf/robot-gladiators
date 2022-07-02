4296dd6a13937a98d953db23d533763815de8c38
nashalysf <nashalysf@gmail.com>
2022-07-02 17:02:04 -0400

    this is the starting code of javasvript

---
+++ assets/js/game.js
@@ -0,0 +1,8 @@
+function fight() {
+  window.alert("The fight has begun!");
+}
+ //fight();
+
+ let playerName = window.prompt("What's your robot's name?");
+ console.log("Our robot's name is" + " "+ playerName);
+ window.alert("Welcome"+" "+ playerName + "!");
